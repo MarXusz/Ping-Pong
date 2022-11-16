1a9b452f98fca2c7fc77363bb6814f6b24e9ab1d
Mariusz Marynowski <105501781+MarXusz@users.noreply.github.com>
2022-11-16 12:47:30 +0100

    Add files via upload

---
+++ MainGame.java
@@ -0,0 +1,10 @@
+import java.awt.*;
+import java.awt.event.*;
+import java.util.*;
+import javax.swing.*;
+
+public class MainGame {
+	public static void main(String[] args) {
+		Frame frame = new Frame();
+	}
+}
